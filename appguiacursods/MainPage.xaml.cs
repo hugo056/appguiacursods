ca485709c7a8f07c3eabb0688098a412f2a52399
hugo056 <weirdfish056@gmail.com>
2022-11-28 08:49:21 -0300

    updated

---
+++ appguiacursods/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appguiacursods
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public static implicit operator Page(MainPage v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
